89f474eb95572aefa4632f526d213399ebca09ab
Deepti Nara <deepti.nara@hotmail.com>
2026-07-29 18:18:41 -0500

    Enabled swagger for all enviorments

--- src/TaskTracker.Api/Program.cs
+++ src/TaskTracker.Api/Program.cs
@@ -24,11 +24,14 @@ builder.Services.AddScoped<ITaskService, TaskService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
